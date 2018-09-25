d42ff22ffd59f1bad2303623b0501d03925de6db
Swington <tomasz.switon@gmail.com>
2018-09-25 09:24:04 +0200

    Created basic prompt and added .gitignore

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from prompt_toolkit import prompt
+from prompt_toolkit import PromptSession
 
 if __name__ == '__main__':
+    session = PromptSession()
+
     try:
         while True:
             answer = prompt('>>> ')
